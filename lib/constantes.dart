ce1b8b8e81e04d8b2d6e69ff98f2cd3430f06656
Anfacibry <jogos.fabriciocintra@gmail.com>
2022-03-30 16:00:25 -0300

    adicionando tudo

---
+++ lib/constantes.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class Cores {
+  static const Color corConfirmar = Color(0xFF75CF55);
+  static const Color corBotaoFlutuante = Color(0xFF2854AA);
+  static const Color corDialogo = Color(0xFFF3F3F3);
+  static const Color corTextDialogo = Color(0xFF6C6B6B);
+  static const Color corBotaoCancelarECaixaTexto = Color(0xFFD5D5D5);
+  static const Color corItensParaFazer = Color(0xFFF1F1F1);
+}
