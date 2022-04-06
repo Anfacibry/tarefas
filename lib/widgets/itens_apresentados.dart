b59714a2d6b9f8d088491b9a81020b11250fa186
Anfacibry <jogos.fabriciocintra@gmail.com>
2022-04-06 10:01:01 -0300

    adicionando itens stateFul

--- lib/widgets/itens_apresentados.dart
+++ lib/widgets/itens_apresentados.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:tarefas/constantes.dart';
 
-class ItensApresentador extends StatelessWidget {
+class ItensApresentador extends StatefulWidget {
   final String item;
   final DateTime data;
   final double altura, largura;
@@ -15,35 +15,79 @@ class ItensApresentador extends StatelessWidget {
       : super(key: key);
 
   @override
+  State<ItensApresentador> createState() => _ItensApresentadorState();
+}
+
+class _ItensApresentadorState extends State<ItensApresentador> {
+  Color corBotoaConfirmar = Cores.corConfirmar;
+  IconData iconConfirmar = Icons.check_circle;
+  @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.only(
-        top: altura * .003,
-        bottom: altura * .003,
+        top: widget.altura * .003,
+        bottom: widget.altura * .003,
       ),
-      child: Container(
-        height: altura * .1,
-        width: largura * .8,
-        child: Padding(
-          padding: const EdgeInsets.all(5),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Text(DateFormat("dd/MM/yyyy - HH:mm").format(data).toString()),
-              Text(
-                item,
-                style: const TextStyle(
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ],
+      child: Row(
+        children: [
+          Container(
+            height: widget.altura * .08,
+            width: widget.largura * .35,
+            child: LayoutBuilder(
+                builder: ((context, constraints) => Padding(
+                      padding:
+                          EdgeInsets.only(left: constraints.maxWidth * .02),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Text(
+                            DateFormat("dd/MM/yyyy - HH:mm")
+                                .format(widget.data)
+                                .toString(),
+                            style: TextStyle(
+                              fontSize: constraints.maxHeight * .2,
+                              fontWeight: FontWeight.w500,
+                              color: Cores.corTextDialogo,
+                            ),
+                          ),
+                          SingleChildScrollView(
+                            scrollDirection: Axis.horizontal,
+                            child: Text(
+                              widget.item,
+                              style: TextStyle(
+                                fontSize: constraints.maxHeight * .4,
+                                fontWeight: FontWeight.bold,
+                                color: Cores.corTextDialogo,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ))),
+            decoration: BoxDecoration(
+              color: Cores.corItensParaFazer,
+              borderRadius: BorderRadius.circular(10),
+            ),
           ),
-        ),
-        decoration: const BoxDecoration(
-          color: Cores.corItensParaFazer,
-        ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.cancel_outlined,
+              size: widget.largura * .04,
+            ),
+          ),
+          IconButton(
+            onPressed: () {
+              setState(() {});
+            },
+            icon: Icon(
+              iconConfirmar,
+              size: widget.largura * .04,
+              color: corBotoaConfirmar,
+            ),
+          ),
+        ],
       ),
     );
   }
