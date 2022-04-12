c238f34c486d0b7e29d2e5fc5128c6fb837de933
Anfacibry <jogos.fabriciocintra@gmail.com>
2022-04-12 16:05:40 -0300

    fixando os itens apresentados

--- lib/components/item.dart
+++ lib/components/item.dart
@@ -1,9 +1,19 @@
+import 'package:flutter/material.dart';
+
+import '../constantes.dart';
+
 class Item {
   final String nomeItem;
   final DateTime dataItem;
+  Color corBotoaConfirmar;
+  Color corContainerItem;
+  IconData iconConfirmar;
 
   Item({
     required this.nomeItem,
     required this.dataItem,
+    this.corBotoaConfirmar = Cores.corTextDialogo,
+    this.corContainerItem = Cores.corItensParaFazer,
+    this.iconConfirmar = Icons.check_circle_outline,
   });
 }
